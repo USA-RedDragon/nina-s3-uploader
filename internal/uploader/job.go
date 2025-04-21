3a447b2963bf45a0ae3af0268bfb6c3239615679
Jacob McSwain <jacob@mcswain.dev>
2025-04-21 15:49:58 -0500

    upload: use s3 manager

--- internal/uploader/job.go
+++ internal/uploader/job.go
@@ -1,7 +1,6 @@
 package uploader
 
 import (
-	"bufio"
 	"context"
 	"log/slog"
 	"os"
@@ -10,21 +9,18 @@ import (
 
 	"github.com/USA-RedDragon/nina-s3-uploader/internal/config"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type uploadJob struct {
-	path     string
-	s3Client *s3.Client
-	config   *config.Config
+	path      string
+	s3Client  *s3.Client
+	s3Manager *manager.Uploader
+	config    *config.Config
 }
 
 func (u *uploadJob) Run() error {
-	stats, err := os.Stat(u.path)
-	if err != nil {
-		slog.Error("failed to stat file", "path", u.path, "error", err)
-		return err
-	}
 	file, err := os.Open(u.path)
 	if err != nil {
 		slog.Error("failed to open file", "path", u.path, "error", err)
@@ -43,11 +39,10 @@ func (u *uploadJob) Run() error {
 	u.path = strings.ReplaceAll(u.path, "\\", "/")
 
 	slog.Debug("uploading file", "path", u.path, "bucket", u.config.S3.Bucket, "prefix", u.config.S3.Prefix)
-	_, err = u.s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket:        aws.String(u.config.S3.Bucket),
-		Key:           aws.String(u.config.S3.Prefix + u.path),
-		Body:          bufio.NewReader(file),
-		ContentLength: aws.Int64(stats.Size()),
+	_, err = u.s3Manager.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(u.config.S3.Bucket),
+		Key:    aws.String(u.config.S3.Prefix + u.path),
+		Body:   file,
 	})
 	if err != nil {
 		slog.Error("failed to upload file", "path", u.path, "error", err)
--- internal/uploader/uploader.go
+++ internal/uploader/uploader.go
@@ -9,14 +9,16 @@ import (
 	"github.com/USA-RedDragon/nina-s3-uploader/internal/config"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type Uploader struct {
-	config   *config.Config
-	s3Client *s3.Client
-	upload   *uploadJob
-	lock     sync.Mutex
+	config    *config.Config
+	s3Client  *s3.Client
+	s3Manager *manager.Uploader
+	upload    *uploadJob
+	lock      sync.Mutex
 }
 
 func NewUploader(cfg *config.Config) (*Uploader, error) {
@@ -25,7 +27,7 @@ func NewUploader(cfg *config.Config) (*Uploader, error) {
 		return nil, fmt.Errorf("failed to load AWS config: %w", err)
 	}
 
-	return &Uploader{
+	ret := &Uploader{
 		config: cfg,
 		s3Client: s3.NewFromConfig(awsCfg, func(o *s3.Options) {
 			o.Region = cfg.S3.Region
@@ -35,7 +37,25 @@ func NewUploader(cfg *config.Config) (*Uploader, error) {
 				o.UsePathStyle = true
 			}
 		}),
-	}, nil
+	}
+
+	ret.s3Manager = manager.NewUploader(ret.s3Client, func(o *manager.Uploader) {
+		o.PartSize = 16 * 1024 * 1024 // 16MB
+		o.LeavePartsOnError = false
+		o.Concurrency = 5
+		o.ClientOptions = []func(*s3.Options){
+			func(c *s3.Options) {
+				c.Region = cfg.S3.Region
+				if cfg.S3.Endpoint != "" {
+					slog.Warn("using custom S3 endpoint", "endpoint", cfg.S3.Endpoint)
+					c.BaseEndpoint = aws.String(cfg.S3.Endpoint)
+					c.UsePathStyle = true
+				}
+			},
+		}
+	})
+
+	return ret, nil
 }
 
 func (u *Uploader) Upload(path string) error {
@@ -44,9 +64,10 @@ func (u *Uploader) Upload(path string) error {
 
 	if u.upload == nil {
 		u.upload = &uploadJob{
-			path:     path,
-			s3Client: u.s3Client,
-			config:   u.config,
+			path:      path,
+			s3Client:  u.s3Client,
+			config:    u.config,
+			s3Manager: u.s3Manager,
 		}
 		err := u.upload.Run()
 		u.upload = nil
